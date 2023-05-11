a977dfa3c867b6fb413fdf91f35b3619b8cd455c
Dr. Peter Dillinger <peter.dillinger@srh.de>
2023-05-11 11:52:06 +0200

    Mehr Kommentar zum Aufbau und Benamung von Methoden hinzugefügt.

--- src/IrgendEineKlasse.java
+++ src/IrgendEineKlasse.java
@@ -1,6 +1,15 @@
 public class IrgendEineKlasse {
 
     /*
+     * Methoden
+     * aka Unterprogramme
+     * aka Subroutinen
+     * aka Prozeduren
+     * aka Funktionen
+     * aka Nachrichten
+     */
+
+    /*
      * Warum brauchen wir Methoden:
      * 1) Um doppelten Programmcode zu vermeiden
      *    -> um nur eine Stelle zu haben, um den Programmcode
@@ -18,6 +27,45 @@ public class IrgendEineKlasse {
         System.out.println("in eineMethode wird gearbeitet");
     }
 
+    // Aufbau von Methoden:
+//  |----------------------------------------------------| Methodenkopf
+//  |-----------| Zusatz/Modifikatoren
+//                |-| Datentyp, was die Methode am Ende liefern soll
+//                    |--------------| Methodenname
+//                                     |-----------------| Parameter
+    public static int berechneZinssatz(String kontoInhaber) {
+//                    |-----------------------------------| Methodensignatur
+        int kundennummer = sucheKundeInDatebank(kontoInhaber);
+        return leseFestgelegtenZinssatzFürKunde(kundennummer);
+    }
+
+    // mehrere Parameter sind durch Komma getrennt
+    public static double berechnePolynom(double x, double koeffizient, double y){
+        return koeffizient * x + y;
+    }
+
+    // muss in der Zukunft gelöst werden
+    private static int leseFestgelegtenZinssatzFürKunde(int kundennummer) {
+        return 0;
+    }
+
+    // muss in der Zukunft gelöst werden (siehe oben)
+    private static int sucheKundeInDatebank(String kontoInhaber) {
+        return 0;
+    }
+//  alles zwischen den geschweiften Klammern {...} ist der Methodenrumpf
+
+    // feine Unterscheidung zwischen den Methoden
+    public static void prozedur() {
+        // Methoden mit void, d.h. ohne Rückgabewert werden auch als
+        // Prozeduren bezeichnet
+    }
+
+    public static int funktion(int x) {
+        // Methoden mit Datentyp berechnen ein Ergebnis (benötigen ein
+        // return Befehl) und werden als Funktionen bezeichnet
+        return x;
+    }
 
     // Das hier "main" ist der Einsprungpunkt in unser Programm
     // main aka start aka init aka setup
@@ -33,5 +81,8 @@ public class IrgendEineKlasse {
         // gib die Zahlen von 0 bis 9 aus
         // eher wichtig: WARUM brauch ich diese 10 Zahlen ???
         for (int i=0; i<20; i++) System.out.println(i);
+
+        double ergebnis = berechnePolynom(17, 4, 0);
+        eineMethode();
     }
 }
